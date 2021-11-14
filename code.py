d72b367d04de241dac53b6bb43ae8ec3e88d2649
Soham Bhavsar <soham.bhavsar@hotmail.com>
2021-11-14 18:44:18 -0500

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,20 @@ import numpy as np
 import cv2
 
 
+video_stream = cv2.VideoCapture('images/overpass.mp4')
+
+# Randomly select 30 frames
+frameIds = video_stream.get(cv2.CAP_PROP_FRAME_COUNT) * np.random.uniform(size=30)
+
+# Store selected frames in an array
+frames = []
+for fid in frameIds:
+    video_stream.set(cv2.CAP_PROP_POS_FRAMES, fid)
+    ret, frame = video_stream.read()
+    frames.append(frame)
+    
+video_stream.release()
+
 medianFrame = np.median(frames, axis=0).astype(dtype=np.uint8)
 grayMedianFrame = cv2.cvtColor(medianFrame, cv2.COLOR_BGR2GRAY)
 graySample=cv2.cvtColor(sample_frame, cv2.COLOR_BGR2GRAY)
